97ef2bf352dc499f6913e229958589e9568e9deb
Daniel Victor <114032607+skidan007@users.noreply.github.com>
2025-05-23 13:57:32 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,20 +1,20 @@
 
-const count = 2;
+// const count = 2;
 
-if (count.length === 0) {
-  console.log("No argument");
-} else if (count === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
-
-// const args = process.argv.slice(2); // Gets actual arguments passed to script
-
-// if (args.length === 0) {
+// if (count.length === 0) {
 //   console.log("No argument");
-// } else if (args.length === 1) {
+// } else if (count === 1) {
 //   console.log("Argument found");
 // } else {
 //   console.log("Arguments found");
 // }
+
+const count = process.argv.slice(2); 
+
+if (count.length === 0) {
+  console.log("No argument");
+} else if (count.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
